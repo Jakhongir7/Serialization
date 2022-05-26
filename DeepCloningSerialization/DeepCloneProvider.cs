d49fe8f63fae8a4338314392d8f9453baa98c4cb
Jakhongir7 <bakhodirovjakhongir@gmail.com>
2022-05-26 12:47:31 +0500

    Task 3

---
+++ DeepCloningSerialization/DeepCloneProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable
+
+namespace DeepCloningSerialization
+{
+    public class CloneHelper
+    {
+        public static T Clone<T>(object objSource)
+        {
+            using var tmpStream = new MemoryStream();
+            var bf = new BinaryFormatter();
+            bf.Serialize(tmpStream, objSource);
+            tmpStream.Position = 0;
+            return (T)bf.Deserialize(tmpStream);
+        }
+    }
+}
---
+++ DeepCloningSerialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using BinarySerialization.Models;
+using DeepCloningSerialization;
+using System;
+using System.Collections.Generic;
+
+var department = new Department
+{
+    DepartmentName = "Managers",
+    Employees = new List<Employee>
+    {
+        new() { EmployeeName = "Jakhongir" },
+        new() { EmployeeName = "Bakhodir" },
+        new() { EmployeeName = "Alisher" },
+        new() { EmployeeName = "Anora" },
+        new() { EmployeeName = "Komila" },
+    }
+};
+
+var clone = CloneHelper.Clone<Department>(department);
+
+Console.WriteLine(department);
+Console.WriteLine(clone);
+department.DepartmentName = "Engineers";
+department.Employees[1].EmployeeName = "NotJakhongir";
+Console.WriteLine(department);
+Console.WriteLine(clone);
